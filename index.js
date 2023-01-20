beca58a1398dd351e3f92309711d4d33b408db6e
Chet25 <chirag1292003@gmail.com>
2023-01-20 11:25:48 +0530

    A simple portfolio static website as a solo challenge from scrimba

--- index.js
+++ index.js
@@ -1,35 +1,66 @@
 
-function NavBar() {
-    return (
-        <nav className="header">
-            <img src="react-logo.png" />
-            <h3>ReactFacts</h3>
-            <h4>React Course - Project 1</h4>
-        </nav>
-    )
-}
-
 function Header() {
     return (
-        <div className="main">
-            <h1>Fun facts about React</h1>
-            <ul className="main-list">
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on GitHub</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
+        <div className="header">
+            <img src="img/profile.svg" className="profile-photo"/>
+            <div className="header-text">
+                <h1>Duan Boomer</h1>
+                <h4>Frontend Developer</h4>
+                <p>duanboomer.github.io</p>
+
+            </div>
+            <div className="button">
+                <div className="button-email">
+                    <img src="img/Icon.png"/>
+                    <p>Email</p>
+                </div>
+
+                <div className="button-linkedin">
+                    <img src="img/Vector.png"/>
+                    <p>LinkedIn</p>
+                </div>
+            </div>
         </div>
     )
 }
 
+function About(){
+    return (
+        <div className="about-section">
+            <h2>About</h2>
+            <p>I am taking my first steps into the world of web development by diving into the a react course offered by freeCodeCamp. This is a solo project in accordance with that course.</p>
+        </div>
+    )
+}
 
+function Interests(){
+    return (
+        <div className="interests-section">
+            <h2>Interests</h2>
+            <p>React , Web Development , Machine Learning , Game Development , Unity , Flutter</p>
+        </div>
+    )
+}
+
+function Footer(){
+    return (
+        <div className="footer">
+            <img src="img/Twitter Icon.png"/>
+            <img src="img/Facebook Icon.png"/>
+            <img src="img/Instagram Icon.png"/>
+            <img src="img/GitHub Icongithubicon.png"/>
+        </div>
+    )
+}
 function App() {
     return (
-        <div className="container">
-            <NavBar />
+        <div >
             <Header />
+            <About />
+            <Interests />
+            <Footer />
+
+
         </div>
     )
 }
